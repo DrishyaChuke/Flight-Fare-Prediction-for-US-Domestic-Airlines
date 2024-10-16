83f04188123c8d1fba92d646e11fc31c9e08bdfc
Drishya Chuke <drishyachuke98@gmail.com>
2024-10-16 22:01:28 +1100

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Assignmet 3 for adv lma',
+    author='Drishya Chuke',
+    license='MIT',
+)
